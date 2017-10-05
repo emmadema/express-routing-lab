844913d4eb530a7eae348b7875a3166281487cc1
emmadema <emma.demango@gmail.com>
2017-10-04 19:30:34 -0600

    draft

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,17 +1,26 @@
+
 //Did you use npm install to
 //add all these packages
-//to our project?
+//to our project? -Done
+
 var express = require('express');
+//requires express to be run
 var app = express();
+//creates and express application
 var bodyParser = require('body-parser');
+//includes body parser in the module
+
+var port = process.env.PORT || 3000;
 
 // How do we 'require' the candyRouter file?
-var candyRouter;
+var myRouter = require('./candyRouter');
 
+//tells the application we want to use body parser middlewear
 app.use(bodyParser.json());
 
 //How do we redirect the /candies path
 //through our candyRouter?
 //Hint: you need app.use
+app.use('/candies', myRouter);
 
-app.listen(3000);
+app.listen(port);
--- starter-code/candyRouter.js
+++ starter-code/candyRouter.js
@@ -1,17 +1,62 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-router = express.Router();
 
-var candies;
+var express = require('express');
+//requires express to be run
+var bodyParser = require('body-parser');
+//include body parse in the module
+var router = express.Router();
+//allows us to change app.get to router.get
+//creates a mini app called router
 
 //What would need to go into candies
 //in order to pass our first test?
+//added an array of candies
+var candies =[
+{"id":1,"name":"Chewing Gum","color":"Red"},
+{"id":2,"name":"Pez","color":"Green"},
+{"id":3,"name":"Marshmallow","color":"Pink"},
+{"id":4,"name":"Candy Stick","color":"Blue"}
+];
 
-router.get('/', function(req,res) {
+
+//INDEX
+router.get('/', function(req, res) {
 	// What would go here? 
 	// Hint: we want all candies in JSON format
+	res.json(candies);
+	router.use(bodyParser.json());
+	console.log('Something is working');
 });
 
 // Fill out the rest of the routes here
+
+//gets just one candy
+//SHOW
+router.get('/:id', function(req, res){
+	router.param('id');
+	router.use(bodyParser.json());
+	console.log('Something is working 2');
+});
+
+//gets a new html form
+//router.get('/new', function(req, res){
+
+//});
+
+//makes a new candy
+//router.post('/', function(req, res){
+
+//});
+
+//updates array with new info
+//router.get('/id:/edit', function(req,res){
+
+//});
+
+//deltes a candy
+ //router.delete('/:id', function(req, res){
+
+ //});
+
+ //app.use("/candy", router);
 
 module.exports = router;
